cfff1534a4a2c9e1b1b7c639621fd7061dac7816
eldamiano93 <szczepaniakdamian93@gmail.com>
2021-08-26 20:58:45 +0200

    zad4

---
+++ src/main/java/pl/sda/Zadanie4/Library.java
@@ -0,0 +1,20 @@
+package pl.sda.Zadanie4;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class Library<T extends MediaCenter> {
+
+    private List<T> mediaList = new ArrayList<>();
+    public void addMedia (T medium) {
+       mediaList.add(medium);
+    }
+
+    public Optional<T> mediaBorrow(String name){
+        return mediaList.stream()
+                .filter(media-> media.getName().equals(name))
+                .findAny();
+    }
+
+}
---
+++ src/main/java/pl/sda/Zadanie4/MediaCenter.java
@@ -0,0 +1,7 @@
+package pl.sda.Zadanie4;
+
+public interface MediaCenter {
+
+    String getName();
+
+}
